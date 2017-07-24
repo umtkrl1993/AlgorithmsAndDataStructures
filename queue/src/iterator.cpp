d9b5823da73aa5ee8a563978d1b22576ec992c24
umtaykrt1993 <umitaykurt1993@gmail.com>
2017-07-24 17:10:12 +0300

    Added some iterator implementations

--- queue/src/iterator.cpp
+++ queue/src/iterator.cpp
@@ -3,9 +3,9 @@
 namespace DataStructures
 {
 
-queue::iterator::iterator( int* arr, int size )
+queue::iterator::iterator( int* arr, int size, int setIndex )
 {
-    m_currentIndex = 0;
+    m_currentIndex = setIndex;
     m_size = size;
     mp_arr = arr;
 }
@@ -21,6 +21,33 @@ void queue::iterator::operator++()
 int queue::iterator::operator*()
 {
     return mp_arr[m_currentIndex];
+}
+
+bool queue::iterator::operator ==( const queue::iterator& it2 )
+{
+	if( m_size != it2.m_size )
+	{
+		return false;
+	}
+
+	if( m_currentIndex != it2.m_currentIndex )
+	{
+		return false;
+	}
+
+	return true;
+
+}
+
+bool queue::iterator::operator !=( const queue::iterator& it2 )
+{
+	if( m_currentIndex != it2.m_currentIndex )
+	{
+		return true;
+	}
+
+	return false;
+	
 }
 
 }
