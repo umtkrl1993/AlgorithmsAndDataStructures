ee282ca2564102284b3cc37a2b93b2ac8734d447
umtaykrt1993 <umitaykurt1993@gmail.com>
2018-04-05 11:35:28 +0300

    cont. with graph implementation

--- graph.cpp
+++ graph.cpp
@@ -8,7 +8,7 @@ class Graph{
 
 		Graph( int vertex_number ){
 
-			this->m_vertex_number = vertex_number;
+			this->m_vertice_number = vertex_number;
 			this->m_vertice_index = 0;
 			initializeGraph();
 		}
@@ -19,10 +19,12 @@ class Graph{
 
 		}
 
+
+		//undirected graph
 		void addEdge( std::string src, std::string dst, int weight ){
 
-			int src_id = m_vertice_map[src];
-			int dst_id = m_vertice_map[dst];
+			int src_id = m_vertice_map_to_id[src];
+			int dst_id = m_vertice_map_to_id[dst];
 
 			graphVertex* src_vertex = mp_graph[src_id];
 
@@ -41,10 +43,9 @@ class Graph{
 			new_vertex->dest = dst_id;
 
 			if( pre == nullptr ){
-
 				src_vertex->next = new_vertex;
-					
 			}
+
 			else{
 				pre->next = new_vertex;
 			}
@@ -53,25 +54,27 @@ class Graph{
 		
 		void addVertex( std::string vertice_name ){
 
-			std::map< std::string , int >::iterator it = m_vertice_map.find( vertice_name );
+			std::map< std::string , int >::iterator it = m_vertice_map_to_id.find( vertice_name );
 
-			if( it == m_vertice_map.end() ){
-				m_vertice_map[vertice_name] = m_vertice_index++;
+			if( it == m_vertice_map_to_id.end() ){
+				m_id_map_to_vertice[m_vertice_index] = vertice_name;
+				m_vertice_map_to_id[vertice_name] = m_vertice_index++;
 			}
 			else{
-				m_vertice_map.erase( it );
+				m_vertice_map_to_id.erase( it );
 			}
 
 		}
 
 		void printGraph(){
 
-			for( int i = 0; i < m_vertex_number; i++ ){
+
+			for( int i = 0; i < m_vertice_number; i++ ){
 
 				graphVertex* current = mp_graph[i]->next;
 
 				while( current != nullptr ){
-					std::cout << "Edge From " << i << " To " << current->dest << std::endl;
+					std::cout << "Edge From " << m_id_map_to_vertice[i] << " To " << m_id_map_to_vertice[current-> dest] << " Distance is " << current->weight << std::endl;
 					current = current->next;
 				}
 
@@ -81,7 +84,7 @@ class Graph{
 
 		void printVertices(){
 
-			for( std::map< std::string, int >:: iterator it = m_vertice_map.begin(); it != m_vertice_map.end(); it++ ){
+			for( std::map< std::string, int >:: iterator it = m_vertice_map_to_id.begin(); it != m_vertice_map_to_id.end(); it++ ){
 				std::cout << it->first << '\n';
 			}
 
@@ -103,27 +106,8 @@ class Graph{
 
 
 	private:
-		void initializeGraph(){
-			
-			mp_graph = new struct graphVertex*[m_vertex_number];
 
-			for( int i = 0; i < m_vertex_number; i++ ){
-				mp_graph[i] = new struct graphVertex;
-				mp_graph[i] -> dest = i;
-			}
-		}
-
-		void expandMemory(){
-
-			graphVertex** temp = nullptr;
-
-			for( int i = 0; i < m_vertex_number; i++ ){
-
-
-
-			}
-
-		}
+		int m_vertice_number;
 
 		typedef struct graphVertex{
 
@@ -138,11 +122,34 @@ class Graph{
 
 		}graphVertex;
 
-		int m_vertex_number;
 
 		graphVertex** mp_graph;
-		std::map< std::string, int > m_vertice_map;
+		std::map< std::string, int > m_vertice_map_to_id;
+		std::map< int, std::string > m_id_map_to_vertice;
 		int m_vertice_index;
+
+		void initializeGraph(){
+			
+			mp_graph = new graphVertex*[m_vertice_number];
+
+			for( int i = 0; i < m_vertice_number; i++ ){
+				mp_graph[i] = new graphVertex;
+				mp_graph[i] -> dest = i;
+			}
+		}
+
+		void expandMemory(){
+
+			graphVertex** temp = nullptr;
+
+			for( int i = 0; i < m_vertice_number; i++ ){
+
+
+
+			}
+
+		}
+
 
 };
 
@@ -158,29 +165,19 @@ int main(){
 	g.addVertex( "Adana" );
 	g.addVertex( "Antalya");
 
-	g.printVertices();
+	g.addEdge( "Ankara", "Istanbul", 500 );
+	g.addEdge( "Ankara", "Izmir", 800 );
+	g.addEdge( "Ankara", "Adana", 400 );
+	g.addEdge( "Izmir", "Istanbul", 450 );
+	g.addEdge( "Izmir", "Antalya", 250 );
+	g.addEdge( "Istanbul", "Antalya", 600);
 
 
-//
-//	g.addEdge( 0 , 3 );
-//	g.addEdge( 0, 4 );
-//	g.addEdge( 0, 5 );
-//	g.addEdge( 0, 6 );
-//	g.addEdge( 1, 2 );
-//	g.addEdge( 2, 4 );
-//	g.addEdge( 3, 9 );
-//	g.addEdge( 4, 8 );
-//	g.addEdge( 4, 6 );
-//	g.addEdge( 6, 0);
-//	g.addEdge( 6,1);
-//	g.addEdge( 6,5);
-//	g.addEdge( 6,6);
-
-	g.printNeigh( 6 );
-//g.addEdge( 5, 7 );
 
 
-	//g.printGraph();
+
+
+	g.printGraph();
 	
 	
 
